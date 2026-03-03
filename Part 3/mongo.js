81df68e624bf6fd8e69ab1b081bf4c3e274f92df
ARTHUR-MORGAN2004 <jsreeharisreelakam@gmail.com>
2026-03-03 11:31:27 +0530

    hi

--- Part 3/mongo.js
+++ Part 3/mongo.js
@@ -6,7 +6,7 @@ if (process.argv.length < 3) {
 }
 
 const password = process.argv[2]
-const url = `mongodb+srv://sooryakiran:${password}@cluster0.wamnpaw.mongodb.net/phonebookApp?retryWrites=true&w=majority`
+const url = `mongodb+srv://Sreehari j:${password}@cluster0.wamnpaw.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
